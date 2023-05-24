a80d53983e31fc4eaa8d30e748008885b50385e4
niccoloruberti <nikiruberti@gmail.com>
2023-05-24 17:05:19 +0200

    esercizio finito

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+//creo un array con tutti i prodotti da acquistare
+let shoppingItems = [
+    'farina',
+    'lievito',
+    'acqua',
+    'pomodoro',
+    'mozzarella',
+    'basilico',
+    'olio'
+]
+
+//dichiaro la variabile che contiene l'elemento del DOM dove dovrò inserire la lista della spesa
+let shoppingList = document.querySelector('.shopping-list');
+
+//creo un ciclo che aggiunge gli elementi dell'array alla lista della spesa
+let contatore = 0;
+while ( contatore < shoppingItems.length) {
+    let listItem = document.createElement('li');
+    listItem.innerText = `${shoppingItems[contatore]}`;
+    shoppingList.append(listItem);
+    contatore++
+}
